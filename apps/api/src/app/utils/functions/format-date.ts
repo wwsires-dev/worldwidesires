eb218c605f972040735a32c37388a5d74c9875d2
Derek Damko <ddamko@wwsires.com>
2022-07-21 08:33:26 -0700

    Created a FormateDate util func that formats dates to international format of YYYY-MM-DD

---
+++ apps/api/src/app/utils/functions/format-date.ts
@@ -0,0 +1,8 @@
+export const FormatDate = (dateStr: string): string => {
+  const date = new Date(dateStr);
+  const day = date.getDay();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return String.prototype.concat(year + "-" + month + "-" + day);
+}
